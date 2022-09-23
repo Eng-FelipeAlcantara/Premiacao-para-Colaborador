35ab6d9e817f750230953b6e086e58fd5eec1409
Felipe Félix de Alcântara <112891826+Eng-FelipeAlcantara@users.noreply.github.com>
2022-09-23 12:32:51 -0300

    Add files via upload
    #Programação que estabelece um aproveitamento mínimo para concessão de premiação a um colaborador de uma empresa.

---
+++ premiação.py
@@ -0,0 +1,7 @@
+#Programação que estabele um aproveitamento mínimo para concessão de premiação a um colaborador de uma empresa.
+nota = float(input("Digite a nota: "))
+
+if nota >= 6.0:
+    print("Prêmio no valor de R$10.000 concedido ao colaborador.")
+else:
+    print("Prêmio não concedido")
